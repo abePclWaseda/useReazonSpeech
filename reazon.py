d9bf1e1260b7f19c3b937421ddee86eedd00e72f
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2025-06-13 14:59:46 +0900

    推論結果をJSON形式で保存する機能を追加

--- reazon.py
+++ reazon.py
@@ -1,35 +1,52 @@
-# from reazonspeech.espnet.asr import load_model, transcribe, audio_from_path
-
-# audio = audio_from_path("dcdd979f47cb788aeb8ef58033d37fff.wav")
-# model = load_model()
-# ret = transcribe(model, audio)
-# print(ret)
-
-# import json
-# from dataclasses import asdict
-# from pathlib import Path
-
-# segments_json = [asdict(seg) for seg in ret.segments]
-
-# out_path = Path("segments.json")
-# out_path.write_text(
-#     json.dumps(segments_json, ensure_ascii=False, indent=2), encoding="utf-8"
-# )
-# print("✅ saved to", out_path)
-
-# -----------------------------------------------------------------------------
 from reazonspeech.nemo.asr import load_model, transcribe, audio_from_path
-
-audio = audio_from_path("/mnt/kiso-qnap3/yuabe/m1/moshi-finetune/data/CallHome/audio/0696.mp3")
-model = load_model()
-ret = transcribe(model, audio)
-
 import json
 from dataclasses import asdict
 from pathlib import Path
 
-sp_tokens = [asdict(sw) for sw in ret.subwords]
+# 推論
+audio = audio_from_path(
+    "/mnt/kiso-qnap3/yuabe/m1/moshi-finetune/data/CallHome/audio/0696.mp3"
+)
+model = load_model()
+ret = transcribe(model, audio)
+ret_dict = {
+    "text": ret.text,
+    "segments": [asdict(seg) for seg in ret.segments],
+    "subwords": [asdict(sw) for sw in ret.subwords],
+    "hypothesis": (
+        ret.hypothesis
+        if isinstance(ret.hypothesis, (str, dict, list, int, float, type(None)))
+        else None
+    ),
+}
 
-out = Path("/mnt/kiso-qnap3/yuabe/m1/moshi-finetune/data/CallHome/text/0696.json")
-out.write_text(json.dumps(sp_tokens, ensure_ascii=False, indent=2), encoding="utf-8")
-print("✅ Saved:", out)
+# 保存
+output_path = Path(
+    "/mnt/kiso-qnap3/yuabe/m1/moshi-finetune/data/CallHome/text/0696_full_ret.json"
+)
+output_path.write_text(
+    json.dumps(ret_dict, ensure_ascii=False, indent=2), encoding="utf-8"
+)
+
+print("✅ Saved:", output_path)
+
+# フォーマット整形（1行ずつ書き出す）
+# out_path = Path("/mnt/kiso-qnap3/yuabe/m1/moshi-finetune/data/CallHome/text/0696.json")
+# with out_path.open("w", encoding="utf-8") as f:
+#     f.write("[\n")
+#     for i, sw in enumerate(ret.subwords):
+#         obj = {
+#             # "speaker": getattr(sw, "speaker", "A"),
+#             "word": sw.token,
+#             "start": sw.start,
+#             "end": sw.end,
+#         }
+#         json_str = json.dumps(obj, ensure_ascii=False)
+#         f.write(f"  {json_str}")
+#         if i != len(ret.subwords) - 1:
+#             f.write(",\n")
+#         else:
+#             f.write("\n")
+#     f.write("]\n")
+
+# print("✅ Saved:", out_path)
